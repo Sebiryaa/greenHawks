bfb9c1b6610381b0b6153e0746fab608d7a122b5
Sebriya Ahmed <sebriyaahmed@dyn219155.cc.lehigh.edu>
2023-05-01 04:32:11 -0400

    lastt

--- app/App.js
+++ app/App.js
@@ -3,7 +3,7 @@ import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
 import "./App.css";
-import { json } from "./json";
+import { json } from "./src/json";
 
 function SurveyComponent() {
     const survey = new Model(json);
